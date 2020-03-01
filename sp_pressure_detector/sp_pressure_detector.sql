f0c3d57ae84764d2e8d44f40ed3ff9337f1325a3
Erik Darling <ErikDarling@users.noreply.github.com>
2020-03-01 09:25:57 -0500

    fix spacing

--- sp_pressure_detector/sp_pressure_detector.sql
+++ sp_pressure_detector/sp_pressure_detector.sql
@@ -11,23 +11,23 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
   
         MIT License
 
-	Permission is hereby granted, free of charge, to any person obtaining a copy
-	of this software and associated documentation files (the "Software"), to deal
-	in the Software without restriction, including without limitation the rights
-	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-	copies of the Software, and to permit persons to whom the Software is
-	furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-	The above copyright notice and this permission notice shall be included in all
-	copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-	SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
     /*
@@ -39,8 +39,8 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
          WHERE c.name = N'remote admin connections' ) = 0
     BEGIN
         SELECT 'This works a lot better on a troublesome server with the DAC enabled' AS message,
-    	       'EXEC sp_configure ''remote admin connections'', 1; RECONFIGURE;' AS command_to_run,
-    	       'https://bit.ly/RemoteDAC' AS how_to_use_the_dac;
+               'EXEC sp_configure ''remote admin connections'', 1; RECONFIGURE;' AS command_to_run,
+               'https://bit.ly/RemoteDAC' AS how_to_use_the_dac;
     END
     
     /*
@@ -79,7 +79,7 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
                 deqmg.grant_time,
                 (deqmg.requested_memory_kb / 1024.) requested_memory_mb,
                 (deqmg.granted_memory_kb / 1024.) granted_memory_mb,
-                (deqmg.ideal_memory_kb / 1024.) ideal_memory_mb,		
+                (deqmg.ideal_memory_kb / 1024.) ideal_memory_mb,        
                 (deqmg.required_memory_kb / 1024.) required_memory_mb,
                 (deqmg.used_memory_kb / 1024.) used_memory_mb,
                 (deqmg.max_used_memory_kb / 1024.) max_used_memory_mb,
@@ -92,7 +92,7 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
                 waits.wait_type,
                 deqmg.reserved_worker_count,
                 deqmg.used_worker_count,
-		        deqp.query_plan
+                deqp.query_plan
     FROM        sys.dm_exec_query_memory_grants AS deqmg
     OUTER APPLY ( SELECT   TOP (1) *
                   FROM     sys.dm_os_waiting_tasks AS dowt
@@ -135,7 +135,7 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
     WHERE      dos.status = N'VISIBLE ONLINE'
     OPTION(MAXDOP 1);
 
-	
+    
     /*Any threadpool waits*/
     SELECT dowt.session_id,
            dowt.wait_duration_ms,
@@ -151,9 +151,9 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
     DECLARE @cool_new_columns BIT = 0;
     
     IF ( SELECT COUNT(*)
-    	 FROM sys.all_columns AS ac 
-    	 WHERE OBJECT_NAME(ac.object_id) = N'dm_exec_requests'
-    	 AND ac.name IN (N'dop', N'parallel_worker_count') ) = 2
+         FROM sys.all_columns AS ac 
+         WHERE OBJECT_NAME(ac.object_id) = N'dm_exec_requests'
+         AND ac.name IN (N'dop', N'parallel_worker_count') ) = 2
     BEGIN
         SET @cool_new_columns = 1;
     END
@@ -164,11 +164,11 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
     SELECT der.session_id,
            DB_NAME(der.database_id) AS database_name,
            der.start_time,
-    	   SUBSTRING(
+           SUBSTRING(
                     dest.text, ( der.statement_start_offset / 2 ) + 1,
                     (( CASE der.statement_end_offset WHEN -1 THEN DATALENGTH(dest.text) ELSE der.statement_end_offset END
                        - der.statement_start_offset ) / 2 ) + 1) AS query_text,
-	   deqp.query_plan,
+       deqp.query_plan,
            der.status,
            der.blocking_session_id,
            der.wait_type,
@@ -180,34 +180,34 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
            der.writes,
            der.logical_reads,
            CASE 
-    	       WHEN der.transaction_isolation_level = 0 THEN ''Unspecified''
-    	       WHEN der.transaction_isolation_level = 1 THEN ''Read Uncommitted''
-    	       WHEN der.transaction_isolation_level = 2 AND EXISTS ( SELECT 1/0 FROM sys.dm_tran_active_snapshot_database_transactions AS trn WHERE der.session_id = trn.session_id AND is_snapshot = 0 ) THEN ''Read Committed Snapshot Isolation''
-    	       WHEN der.transaction_isolation_level = 2 AND NOT EXISTS ( SELECT 1/0 FROM sys.dm_tran_active_snapshot_database_transactions AS trn WHERE der.session_id = trn.session_id AND is_snapshot = 0 ) THEN ''Read Committed''
-    	       WHEN der.transaction_isolation_level = 3 THEN ''Repeatable Read''
-    	       WHEN der.transaction_isolation_level = 4 THEN ''Serializable''
-    	       WHEN der.transaction_isolation_level = 5 THEN ''Snapshot''
-    	       ELSE ''???''
-    	   END AS transaction_isolation_level ,
+               WHEN der.transaction_isolation_level = 0 THEN ''Unspecified''
+               WHEN der.transaction_isolation_level = 1 THEN ''Read Uncommitted''
+               WHEN der.transaction_isolation_level = 2 AND EXISTS ( SELECT 1/0 FROM sys.dm_tran_active_snapshot_database_transactions AS trn WHERE der.session_id = trn.session_id AND is_snapshot = 0 ) THEN ''Read Committed Snapshot Isolation''
+               WHEN der.transaction_isolation_level = 2 AND NOT EXISTS ( SELECT 1/0 FROM sys.dm_tran_active_snapshot_database_transactions AS trn WHERE der.session_id = trn.session_id AND is_snapshot = 0 ) THEN ''Read Committed''
+               WHEN der.transaction_isolation_level = 3 THEN ''Repeatable Read''
+               WHEN der.transaction_isolation_level = 4 THEN ''Serializable''
+               WHEN der.transaction_isolation_level = 5 THEN ''Snapshot''
+               ELSE ''???''
+           END AS transaction_isolation_level ,
            der.granted_query_memory'
-    	   + CASE WHEN @cool_new_columns = 1
-    		      THEN N',
+           + CASE WHEN @cool_new_columns = 1
+                  THEN N',
            der.dop,
            der.parallel_worker_count'
-    	          ELSE N''
-    	     END
-    	   + N'
+                  ELSE N''
+             END
+           + N'
     FROM sys.dm_exec_requests AS der
     CROSS APPLY sys.dm_exec_sql_text(der.plan_handle) AS dest
     CROSS APPLY sys.dm_exec_query_plan(der.plan_handle) AS deqp
     WHERE der.session_id <> @@SPID
     AND der.session_id >= 50
     ORDER BY ' + CASE WHEN @cool_new_columns = 1
-    		          THEN N'der.parallel_worker_count DESC
-    			     OPTION(MAXDOP 1);'
-    	              ELSE N'der.cpu_time DESC
-    			     OPTION(MAXDOP 1);'
-    		 END
+                      THEN N'der.parallel_worker_count DESC
+                     OPTION(MAXDOP 1);'
+                      ELSE N'der.cpu_time DESC
+                     OPTION(MAXDOP 1);'
+             END
     
     EXEC sys.sp_executesql @sql;
 
