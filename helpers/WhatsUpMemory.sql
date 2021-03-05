75cb8ec9e500f5ffb5a8805cb5989bac109e4eab
Erik Darling <2136037+erikdarlingdata@users.noreply.github.com>
2021-03-05 16:02:43 -0500

    Update WhatsUpMemory.sql

--- helpers/WhatsUpMemory.sql
+++ helpers/WhatsUpMemory.sql
@@ -35,37 +35,97 @@ GO
 ALTER VIEW dbo.WhatsUpMemory
 AS
 SELECT TOP (2147483647)
-       N'WhatsUpMemory' AS view_name,
-       DB_NAME() AS database_name,
-       SCHEMA_NAME(o.schema_id) AS schema_name,
-       o.name AS object_name,
-       i.name AS index_name,
-       SUM(CASE WHEN au.type IN (1,3) 
+    N'WhatsUpMemory' AS view_name,
+    DB_NAME() AS database_name,
+    SCHEMA_NAME(x.schema_id) AS schema_name,
+    x.object_name,
+    x.index_name,    
+    in_row_pages_mb = 
+        SUM
+        (
+            CASE 
+                WHEN x.type IN (1, 3) 
                 THEN 1 
                 ELSE 0 
-           END) * 8. / 1024. AS in_row_pages_mb,
-       SUM(CASE WHEN au.type = 2 
+            END
+        ) * 8. / 1024.,
+    lob_pages_mb = 
+        SUM
+        (
+            CASE 
+                WHEN x.type = 2 
                 THEN 1 
                 ELSE 0 
-           END) * 8. / 1024. AS lob_pages_mb,
-       COUNT_BIG(*) AS buffer_cache_pages_total
-FROM sys.dm_os_buffer_descriptors AS obd
-INNER JOIN sys.allocation_units AS au
-    ON au.allocation_unit_id = obd.allocation_unit_id
-INNER JOIN sys.partitions AS p
-    ON ( ( au.container_id = p.hobt_id AND au.type IN ( 1, 3 ))
-      OR ( au.container_id = p.partition_id AND au.type IN ( 2 )))
-INNER JOIN sys.objects AS o
-    ON p.object_id = o.object_id
-INNER JOIN sys.indexes AS i
-    ON  o.object_id = i.object_id
-    AND p.index_id = i.index_id
-WHERE      au.type > 0
-AND        o.is_ms_shipped = 0
-AND        obd.database_id = DB_ID()
-GROUP BY   SCHEMA_NAME(o.schema_id), 
-           i.name, 
-           o.name
-ORDER BY   COUNT_BIG(*) DESC;
+            END
+        ) * 8. / 1024.,
+    buffer_cache_pages_total = 
+        COUNT_BIG(*)
+FROM 
+(
+    SELECT       
+        o.schema_id,
+        o.name AS object_name,
+        i.name AS index_name,
+        au.type,
+        au.allocation_unit_id 
+    FROM sys.allocation_units AS au
+    INNER JOIN sys.partitions AS p
+        ON au.container_id = p.hobt_id 
+        AND au.type =1
+    INNER JOIN sys.objects AS o
+        ON p.object_id = o.object_id
+    INNER JOIN sys.indexes AS i
+        ON  o.object_id = i.object_id
+        AND p.index_id = i.index_id
+    WHERE au.type > 0
+    AND   o.is_ms_shipped = 0
+    
+    UNION ALL
+    
+    SELECT       
+        o.schema_id,
+        o.name AS object_name,
+        i.name AS index_name,
+        au.type,
+        au.allocation_unit_id 
+    FROM sys.allocation_units AS au
+    INNER JOIN sys.partitions AS p
+        ON au.container_id = p.hobt_id 
+        AND au.type = 3
+    INNER JOIN sys.objects AS o
+        ON p.object_id = o.object_id
+    INNER JOIN sys.indexes AS i
+        ON  o.object_id = i.object_id
+        AND p.index_id = i.index_id
+    WHERE au.type > 0
+    AND   o.is_ms_shipped = 0
+    
+    UNION ALL
+    
+    SELECT       
+        o.schema_id,
+        o.name AS object_name,
+        i.name AS index_name,
+        au.type,
+        au.allocation_unit_id 
+    FROM sys.allocation_units AS au
+    INNER JOIN sys.partitions AS p
+        ON au.container_id = p.partition_id 
+        AND au.type = 2
+    INNER JOIN sys.objects AS o
+        ON p.object_id = o.object_id
+    INNER JOIN sys.indexes AS i
+        ON  o.object_id = i.object_id
+        AND p.index_id = i.index_id
+    WHERE au.type > 0
+    AND   o.is_ms_shipped = 0
+) AS x
+JOIN sys.dm_os_buffer_descriptors AS obd
+    ON x.allocation_unit_id = obd.allocation_unit_id
+GROUP BY 
+    SCHEMA_NAME(x.schema_id), 
+    x.object_name, 
+    x.index_name
+ORDER BY COUNT_BIG(*) DESC;
 GO
 
