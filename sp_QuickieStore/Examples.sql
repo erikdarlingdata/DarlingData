570beafe132154a708d91e093756332926d63f7a
Erik Darling <ErikDarling@users.noreply.github.com>
2021-05-04 14:46:26 -0400

    Update examples

--- sp_QuickieStore/Examples.sql
+++ sp_QuickieStore/Examples.sql
@@ -1,36 +1,64 @@
-/*
-Example calls:
+﻿/*
+███████╗██╗  ██╗ █████╗ ███╗   ███╗██████╗ ██╗     ███████╗
+██╔════╝╚██╗██╔╝██╔══██╗████╗ ████║██╔══██╗██║     ██╔════╝
+█████╗   ╚███╔╝ ███████║██╔████╔██║██████╔╝██║     █████╗  
+██╔══╝   ██╔██╗ ██╔══██║██║╚██╔╝██║██╔═══╝ ██║     ██╔══╝  
+███████╗██╔╝ ██╗██║  ██║██║ ╚═╝ ██║██║     ███████╗███████╗
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝╚══════╝
+                                                           
+ ██████╗ █████╗ ██╗     ██╗     ███████╗                   
+██╔════╝██╔══██╗██║     ██║     ██╔════╝                   
+██║     ███████║██║     ██║     ███████╗                   
+██║     ██╔══██║██║     ██║     ╚════██║                   
+╚██████╗██║  ██║███████╗███████╗███████║                   
+ ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
+*/
 
-Get help!
-
+--Get help!
 EXEC dbo.sp_QuickieStore
     @help = 1;
 
-Find top 10 sorted by memory 
 
+--Find top 10 sorted by memory 
 EXEC dbo.sp_QuickieStore
     @database_name = 'StackOverflow2013',
     @sort_order = 'memory',
     @top = 10;              
 
-Search for a specific query_id
 
+--Search for specific query_ids
 EXEC dbo.sp_QuickieStore
     @database_name = 'StackOverflow2013',
     @top = 10,
-    @query_id = 13977;    
+    @include_query_ids = '13977, 13978';    
 
-Search for a specific plan_id
 
+--Search for specific plan_ids
 EXEC dbo.sp_QuickieStore
     @database_name = 'StackOverflow2013',
     @sort_order = 'memory',
     @top = 10,
     @start_date = '20210320',
-    @plan_id = 1896;    
+    @include_plan_ids = '1896, 1897';
 
-Search for queries within a date range
+    
+--Ignore for specific query_ids
+EXEC dbo.sp_QuickieStore
+    @database_name = 'StackOverflow2013',
+    @top = 10,
+    @ignore_query_ids = '13977, 13978';    
 
+
+--Ignore for specific plan_ids
+EXEC dbo.sp_QuickieStore
+    @database_name = 'StackOverflow2013',
+    @sort_order = 'memory',
+    @top = 10,
+    @start_date = '20210320',
+    @ignore_plan_ids = '1896, 1897'; 
+
+
+--Search for queries within a date range
 EXEC dbo.sp_QuickieStore
     @database_name = 'StackOverflow2013',
     @sort_order = 'memory',
@@ -39,47 +67,55 @@ EXEC dbo.sp_QuickieStore
     @end_date = '20210321';              
 
 
-Search for queries with a minimum execution count
-
+--Search for queries with a minimum execution count
 EXEC dbo.sp_QuickieStore
     @database_name = 'StackOverflow2013',
     @top = 10,
     @execution_count = 10;
 
-Search for queries over a specific duration
 
+--Search for queries over a specific duration
 EXEC dbo.sp_QuickieStore
     @database_name = 'StackOverflow2013',
     @top = 10,
     @duration_ms = 10000;
 
-Search for a specific stored procedure
 
+--Search for a specific stored procedure
 EXEC dbo.sp_QuickieStore
     @database_name = 'StackOverflow2013',
     @procedure_name = 'top_percent_sniffer';   
 
 
-Search for specific query text
-
+--Search for specific query tex
 EXEC dbo.sp_QuickieStore
     @database_name = 'StackOverflow2013',
     @query_text_search = 'WITH Comment'
 
-Use expert mode to return additional columns
 
+--Use expert mode to return additional columns
 EXEC dbo.sp_QuickieStore
     @database_name = 'StackOverflow2013',
     @sort_order = 'memory',
     @top = 10,
     @expert_mode = 1;              
 
-Use format output to add commas to larger numbers
 
+--Use format output to add commas to larger numbers
 EXEC dbo.sp_QuickieStore
     @database_name = 'StackOverflow2013',
     @sort_order = 'memory',
     @top = 10,
     @format_output = 1;
 
-*/
+
+--Troubleshoot performance
+EXEC dbo.sp_QuickieStore
+    @database_name = 'StackOverflow2013',
+    @troubleshoot_performance = 1;
+
+
+--Debug dynamic SQL and temp table contents
+EXEC dbo.sp_QuickieStore
+    @database_name = 'StackOverflow2013',
+    @debug = 1;
