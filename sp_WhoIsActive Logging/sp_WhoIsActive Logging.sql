2376fc97c0eec0b93483a27aaa95b290d0b3dd2d
Erik Darling <2136037+erikdarlingdata@users.noreply.github.com>
2022-08-27 10:55:34 -0400

    Tiny tidy
    Some misplaced spaces.

--- sp_WhoIsActive Logging/sp_WhoIsActive Logging.sql
+++ sp_WhoIsActive Logging/sp_WhoIsActive Logging.sql
@@ -1,3 +1,32 @@
+/*
+
+Copyright 2022 Darling Data, LLC
+https://www.erikdarlingdata.com/
+
+This will log sp_WhoIsActive to a table.
+It will create a new table every day,
+so you don''t have a gigantic table to sift through 
+forever and ever or until your drive fills up.
+
+If you need to get or update sp_WhoIsActive: 
+https://github.com/amachanic/sp_whoisactive
+(C) 2007-2022, Adam Machanic
+
+If you get an error message that @get_memory_info
+isn''t a valid parameter, that''s a pretty good
+sign you need to update.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 USE msdb;
 GO
 
