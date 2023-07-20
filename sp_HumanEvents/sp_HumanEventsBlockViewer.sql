d602eb56f48529ac1408cd5ed5cf5422e0024b45
Michael J Swart <mjswart@gmail.com>
2023-07-19 21:34:12 -0400

    Handle unresolved blockers to make blocking tree not null

--- sp_HumanEvents/sp_HumanEventsBlockViewer.sql
+++ sp_HumanEvents/sp_HumanEventsBlockViewer.sql
@@ -1068,7 +1068,7 @@ PERSISTED;
 
 ALTER TABLE #blocked
 ADD blocking_desc AS 
-    '(' + CAST(blocking_spid AS VARCHAR(max)) + ':' + CAST(blocking_ecid AS VARCHAR(max)) + ')' PERSISTED,
+    ISNULL('(' + CAST(blocking_spid AS VARCHAR(max)) + ':' + CAST(blocking_ecid AS VARCHAR(max)) + ')', 'unresolved process') PERSISTED,
     blocked_desc AS
     '(' + CAST(blocked_spid AS VARCHAR(max)) + ':' + CAST(blocked_ecid AS VARCHAR(max)) + ')' PERSISTED;
 
@@ -1140,7 +1140,7 @@ PERSISTED;
 
 ALTER TABLE #blocking
 ADD blocking_desc AS 
-    '(' + CAST(blocking_spid AS VARCHAR(max)) + ':' + CAST(blocking_ecid AS VARCHAR(max)) + ')' PERSISTED,
+    ISNULL('(' + CAST(blocking_spid AS VARCHAR(max)) + ':' + CAST(blocking_ecid AS VARCHAR(max)) + ')', 'unresolved process') PERSISTED,
     blocked_desc AS
     '(' + CAST(blocked_spid AS VARCHAR(max)) + ':' + CAST(blocked_ecid AS VARCHAR(max)) + ')' PERSISTED;
     
