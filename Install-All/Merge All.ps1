be33b2631aefd15a04d13b0d4fcc5f48e2f548bc
Erik Darling <2136037+erikdarlingdata@users.noreply.github.com>
2023-11-20 14:29:04 -0500

    PS updates
    Change encoding to UTF-8

--- Install-All/Merge All.ps1
+++ Install-All/Merge All.ps1
@@ -1,7 +1,6 @@
-$FilePath = "C:\Users\edarl\OneDrive\Documents\GitHub\DarlingData"
-Get-ChildItem -Path "$FilePath" -Filter "sp_*" |
+Get-ChildItem -Path "C:\Users\edarl\OneDrive\Documents\GitHub\DarlingData" -Filter "sp_*" |
 Where-Object { $_.FullName -notlike "*sp_WhoIsActive*" } |
 ForEach-Object { Get-ChildItem $_.FullName |  
 Where-Object { $_.Name -like "sp_*" -and $_.Name -notlike "sp_Human Events Agent*" } } | 
-ForEach-Object { Get-Content $_.FullName -Encoding Unicode } | 
-Set-Content -Path "$FilePath\Install-All\DarlingData.sql" -Encoding Unicode -Force
+ForEach-Object { Get-Content $_.FullName -Encoding UTF8 } | 
+Set-Content -Path "$FilePath\Install-All\DarlingData.sql" -Encoding UTF8 -Force
