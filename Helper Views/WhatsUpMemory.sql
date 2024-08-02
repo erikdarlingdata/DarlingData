c83c1d403f710c80137fcb2ac2b05b8e5270a091
Erik Darling <2136037+erikdarlingdata@users.noreply.github.com>
2024-08-02 10:59:27 -0400

    WUM
    Attempting to get better performance out of WhatsUpMemory. It is quite annoying that sys.objects and sys.indexes are not parallelizable, and force the entire plan single threaded. i wonder if this might be better as a MSTVF.

--- Helper Views/WhatsUpMemory.sql
+++ Helper Views/WhatsUpMemory.sql
@@ -54,8 +54,7 @@ SELECT TOP (2147483647)
         'WhatsUpMemory',
     database_name =
         DB_NAME(),
-    schema_name =
-        SCHEMA_NAME(x.schema_id),
+    x.schema_name,
     x.object_name,
     x.index_name,
     in_row_pages_mb =
@@ -78,10 +77,12 @@ SELECT TOP (2147483647)
         ) * 8. / 1024.,
     buffer_cache_pages_total =
         COUNT_BIG(*)
-FROM
+FROM sys.dm_os_buffer_descriptors AS obd
+INNER HASH JOIN
 (
     SELECT
-        o.schema_id,
+        schema_name = 
+            s.name,
         object_name =
             o.name,
         index_name =
@@ -90,20 +91,23 @@ FROM
         au.allocation_unit_id
     FROM sys.allocation_units AS au
     JOIN sys.partitions AS p
-      ON au.container_id = p.hobt_id
+      ON  au.container_id = p.hobt_id
       AND au.type =1
     JOIN sys.objects AS o
       ON p.object_id = o.object_id
     JOIN sys.indexes AS i
       ON  o.object_id = i.object_id
       AND p.index_id = i.index_id
+    JOIN sys.schemas AS s
+      ON o.schema_id = s.schema_id
     WHERE au.type > 0
     AND   o.is_ms_shipped = 0
 
     UNION ALL
 
     SELECT
-        o.schema_id,
+        schema_name = 
+            s.name,
         object_name =
             o.name,
         index_name =
@@ -112,20 +116,23 @@ FROM
         au.allocation_unit_id
     FROM sys.allocation_units AS au
     JOIN sys.partitions AS p
-      ON au.container_id = p.hobt_id
+      ON  au.container_id = p.hobt_id
       AND au.type = 3
     JOIN sys.objects AS o
       ON p.object_id = o.object_id
     JOIN sys.indexes AS i
       ON  o.object_id = i.object_id
       AND p.index_id = i.index_id
+    JOIN sys.schemas AS s
+      ON o.schema_id = s.schema_id
     WHERE au.type > 0
     AND   o.is_ms_shipped = 0
 
     UNION ALL
 
     SELECT
-        o.schema_id,
+        schema_name = 
+            s.name,
         object_name =
             o.name,
         index_name =
@@ -134,20 +141,21 @@ FROM
         au.allocation_unit_id
     FROM sys.allocation_units AS au
     JOIN sys.partitions AS p
-      ON au.container_id = p.partition_id
+      ON  au.container_id = p.partition_id
       AND au.type = 2
     JOIN sys.objects AS o
       ON p.object_id = o.object_id
     JOIN sys.indexes AS i
       ON  o.object_id = i.object_id
       AND p.index_id = i.index_id
+    JOIN sys.schemas AS s
+      ON o.schema_id = s.schema_id
     WHERE au.type > 0
     AND   o.is_ms_shipped = 0
 ) AS x
-JOIN sys.dm_os_buffer_descriptors AS obd
-    ON x.allocation_unit_id = obd.allocation_unit_id
+  ON x.allocation_unit_id = obd.allocation_unit_id
 GROUP BY
-    SCHEMA_NAME(x.schema_id),
+    x.schema_name,
     x.object_name,
     x.index_name
 ORDER BY 
