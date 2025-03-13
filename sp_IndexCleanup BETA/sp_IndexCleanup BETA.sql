174d204a3f124fab5a631ba221f832dd769e0800
Erik Darling <2136037+erikdarlingdata@users.noreply.github.com>
2025-03-13 14:19:37 -0400

    Update sp_IndexCleanup BETA.sql

--- sp_IndexCleanup BETA/sp_IndexCleanup BETA.sql
+++ sp_IndexCleanup BETA/sp_IndexCleanup BETA.sql
@@ -3238,7 +3238,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     END;
 
     INSERT INTO 
-        RAISERROR('Generating #index_reporting_stats insert, TABLE', 0, 0) WITH NOWAIT;
+        #index_reporting_stats
     WITH
         (TABLOCK)
     (
