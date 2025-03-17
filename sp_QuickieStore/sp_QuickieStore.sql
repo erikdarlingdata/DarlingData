d8643e93e4c782614868328c107cbcbade327efa
Erik Darling <2136037+erikdarlingdata@users.noreply.github.com>
2025-03-17 11:13:57 -0400

    Update sp_QuickieStore.sql

--- sp_QuickieStore/sp_QuickieStore.sql
+++ sp_QuickieStore/sp_QuickieStore.sql
@@ -8898,7 +8898,8 @@ BEGIN
                         result = 'Availability Group information is empty';
                 END;
             END;
-        END; /*End 2022 views*/
+        END;
+    END; /*End 2022 views*/
 
     IF @expert_mode = 1
     BEGIN
@@ -9740,8 +9741,7 @@ BEGIN
             @sql;
     END;
 
-    END; 
-END; /*End Expert Mode*/;
+END; /*End Expert Mode*/
 
 IF @query_store_trouble = 1
 BEGIN
