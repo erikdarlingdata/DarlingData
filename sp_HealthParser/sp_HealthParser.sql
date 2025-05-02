aacc21aa242de33004c002a7249b0ff289830f35
Erik Darling <2136037+erikdarlingdata@users.noreply.github.com>
2025-05-02 13:02:40 -0400

    Update sp_HealthParser.sql

--- sp_HealthParser/sp_HealthParser.sql
+++ sp_HealthParser/sp_HealthParser.sql
@@ -202,9 +202,9 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     END;
 
     DECLARE
-        @sql nvarchar(max) =
+        @sql nvarchar(MAX) =
             N'',
-        @params nvarchar(max) =
+        @params nvarchar(MAX) =
             N'@start_date datetimeoffset(7),
               @end_date datetimeoffset(7)',
         @azure bit  =
@@ -4203,7 +4203,8 @@ END;
             END;
 
             /* For ignored errors, only display to client */
-            IF @log_to_table = 0
+            IF  @log_to_table = 0
+            AND @debug = 1
             BEGIN
                 SELECT
                     error_numbers_ignored =
